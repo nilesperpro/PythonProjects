3024c39eaa051ada829594c8d20439de73c595e8
Niles <nileswanderson@gmail.com>
2019-12-31 10:40:44 -0700

    Started some things that might be in the right direction

--- VideoScraper/scraper.py
+++ VideoScraper/scraper.py
@@ -2,6 +2,11 @@ import requests
 from lxml.html import fromstring
 from bs4 import BeautifulSoup
 
-target = input("Enter the website you would like to Crawl: ")
+# need to setup and test login from within python to see if Cookie is returned
+loginUrl = "https://api.eliteguardtraining.com/auth/login"
+creds = {"username":"wotesab628@wmail1.com","password":"password"}
 
-print(target)
+response = requests.post(loginUrl, data=creds)
+text = response.text
+html = BeautifulSoup(text, 'html.parser')
+print(text)
